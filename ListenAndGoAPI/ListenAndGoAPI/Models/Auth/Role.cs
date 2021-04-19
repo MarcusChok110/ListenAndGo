a301a3e460f38109108df0459148e090605e138a
Marcus Chok <marcus.chok110@gmail.com>
2021-04-18 21:33:48 -0400

    Create extended models for identity users and roles

---
+++ ListenAndGoAPI/ListenAndGoAPI/Models/Auth/Role.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace ListenAndGoAPI.Models.Auth
+{
+    public class Role : IdentityRole<int>
+    {
+    }
+}
---
+++ ListenAndGoAPI/ListenAndGoAPI/Models/Auth/User.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace ListenAndGoAPI.Models.Auth
+{
+    public class User : IdentityUser<int>
+    {
+    }
+}
