90f157d3dc20467d3ae79279680fc6ab6fb1a27d
Marcus Chok <marcus.chok110@gmail.com>
2021-04-24 22:07:17 -0400

    Add migration for automatically generated DateCreated for playlists and database snapshot

---
+++ ListenAndGoAPI/ListenAndGoAPI/Migrations/20210423194537_AddDefaultDateCreatedForPlaylists.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ListenAndGoAPI.Migrations
+{
+    public partial class AddDefaultDateCreatedForPlaylists : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Playlists",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Playlists",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+        }
+    }
+}
