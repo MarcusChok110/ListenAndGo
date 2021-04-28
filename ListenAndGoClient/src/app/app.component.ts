2f8ddddc9993ceb5a045a76540602247f9a1d551
Marcus Chok <marcus.chok110@gmail.com>
2021-04-28 11:37:03 -0400

    Add user information fetching on initial load

--- ListenAndGoClient/src/app/app.component.ts
+++ ListenAndGoClient/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './core/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ListenAndGoClient';
+export class AppComponent implements OnInit {
+  public title = 'ListenAndGoClient';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.populate();
+  }
 }
