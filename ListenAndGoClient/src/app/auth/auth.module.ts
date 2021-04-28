f8bff185e6c2879e47a626c9d5106033a26348a1
Marcus Chok <marcus.chok110@gmail.com>
2021-04-28 11:31:23 -0400

    Add new necessary imports for snackbars and guards

--- ListenAndGoClient/src/app/auth/auth.module.ts
+++ ListenAndGoClient/src/app/auth/auth.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { FormComponent } from './form/form.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { CoreModule } from '../core/core.module';
+import { LoggedOutGuard } from './logged-out.guard';
 
 @NgModule({
-  declarations: [AuthComponent, FormComponent],
+  declarations: [AuthComponent],
   imports: [
     SharedModule,
+    CoreModule,
     AuthRoutingModule,
     MatInputModule,
     MatButtonModule,
@@ -21,6 +24,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
     MatRadioModule,
     MatCardModule,
     MatGridListModule,
+    MatSnackBarModule,
   ],
+  providers: [LoggedOutGuard],
 })
 export class AuthModule {}
