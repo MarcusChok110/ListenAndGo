a5e2f9294a539453772d228873fd87422558cd48
Marcus Chok <marcus.chok110@gmail.com>
2021-04-28 11:37:35 -0400

    Update providers with new api service name and new auth guard

--- ListenAndGoClient/src/app/core/core.module.ts
+++ ListenAndGoClient/src/app/core/core.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { ApiService } from './services/api.service';
+import { ApiHttpService } from './services/api-http.service';
 import { PlaylistService } from './services/playlist.service';
 import { UserService } from './services/user.service';
 import { JwtService } from './services/jwt.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpTokenInterceptor } from './interceptors/http-token.interceptor';
+import { AuthGuard } from './services/auth.guard';
 
 @NgModule({
   declarations: [],
   imports: [SharedModule],
   providers: [
-    ApiService,
+    ApiHttpService,
     JwtService,
     PlaylistService,
     UserService,
     { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
+    AuthGuard,
   ],
 })
 export class CoreModule {}
