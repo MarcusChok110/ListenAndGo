369cafa943fedfd8d594fa748a54a69a43718494
Marcus Chok <marcus.chok110@gmail.com>
2021-04-29 11:29:11 -0400

    Fix issue where exports were not all in imports

--- ListenAndGoClient/src/app/shared/shared.module.ts
+++ ListenAndGoClient/src/app/shared/shared.module.ts
@@ -4,19 +4,26 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [ConfirmDialogComponent],
-  imports: [CommonModule, RouterModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
   exports: [
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatDialogModule,
     MatButtonModule,
+    MatDialogModule,
     ConfirmDialogComponent,
   ],
 })
