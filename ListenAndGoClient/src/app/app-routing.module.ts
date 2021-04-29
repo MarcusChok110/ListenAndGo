b651e71fc996e7090ffbdf7408fda34de0f4ba2e
Marcus Chok <marcus.chok110@gmail.com>
2021-04-29 11:22:09 -0400

    Update routes for playlist and search components

--- ListenAndGoClient/src/app/app-routing.module.ts
+++ ListenAndGoClient/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { AuthGuard } from './core/services/auth.guard';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'playlist/:playlistId',
+    component: PlaylistComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
