7b37198e7947bf5ee29806a3a22a06a458cca31f
Marcus Chok <marcus.chok110@gmail.com>
2021-04-29 11:15:32 -0400

    Update shared module with confirm dialog and common material modules

--- ListenAndGoClient/src/app/shared/shared.module.ts
+++ ListenAndGoClient/src/app/shared/shared.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
-  exports: [CommonModule, RouterModule, ReactiveFormsModule],
+  declarations: [ConfirmDialogComponent],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, HttpClientModule],
+  exports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatButtonModule,
+    ConfirmDialogComponent,
+  ],
 })
 export class SharedModule {}
