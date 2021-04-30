c324e274d5ff6e760150cf27092c1a8accd45c4a
Marcus Chok <marcus.chok110@gmail.com>
2021-04-30 11:26:26 -0400

    Remove unnecessary endpoints

--- ListenAndGoAPI/ListenAndGoAPI/Controllers/AuthController.cs
+++ ListenAndGoAPI/ListenAndGoAPI/Controllers/AuthController.cs
@@ -16,15 +16,12 @@ namespace ListenAndGoAPI.Controllers
     {
         private readonly IMapper _mapper;
         private readonly UserManager<User> _userManager;
-        private readonly RoleManager<Role> _roleManager;
         private readonly IJwtService _jwtService;
 
-        public AuthController(IMapper mapper, UserManager<User> userManager,
-            RoleManager<Role> roleManager, IJwtService jwtService)
+        public AuthController(IMapper mapper, UserManager<User> userManager, IJwtService jwtService)
         {
             _mapper = mapper;
             _userManager = userManager;
-            _roleManager = roleManager;
             _jwtService = jwtService;
         }
 
@@ -32,48 +29,24 @@ namespace ListenAndGoAPI.Controllers
         public async Task<ActionResult> SignUp(UserAuthDTO userAuthDto)
         {
             var user = _mapper.Map<UserAuthDTO, User>(userAuthDto);
-            var userCreateResult = await _userManager.CreateAsync(user, userAuthDto.Password);
+            var userSignUpResult = await _userManager.CreateAsync(user, userAuthDto.Password);
 
-            return userCreateResult.Succeeded
+            return userSignUpResult.Succeeded
                 ? Created(string.Empty, string.Empty)
-                : Problem(userCreateResult.Errors.First().Description, null, 500);
+                : Problem(userSignUpResult.Errors.First().Description, null, 500);
         }
 
         [HttpPost("SignIn")]
         public async Task<ActionResult> SignIn(UserAuthDTO userAuthDto)
         {
             var user = _userManager.Users.SingleOrDefault(u => u.UserName == userAuthDto.Username);
-            if (user is null) return NotFound("Username not found");
+            if (user is null) return NotFound("Username or password incorrect");
 
             var userSignInResult = await _userManager.CheckPasswordAsync(user, userAuthDto.Password);
 
             return userSignInResult
                 ? Ok(new {jwtToken = _jwtService.GenerateJwt(user, await _userManager.GetRolesAsync(user))})
-                : BadRequest("Username or password incorrect");
-        }
-
-        [HttpPost("Roles")]
-        public async Task<ActionResult> CreateRole(string roleName)
-        {
-            if (string.IsNullOrWhiteSpace(roleName)) return BadRequest("Role name not provided");
-
-            var newRole = new Role {Name = roleName};
-            var roleResult = await _roleManager.CreateAsync(newRole);
-
-            return roleResult.Succeeded
-                ? Ok()
-                : Problem(roleResult.Errors.First().Description, null, 500);
-        }
-
-        [HttpPost("User/{username}/Role")]
-        public async Task<ActionResult> AddUserToRole(string username, [FromBody] string roleName)
-        {
-            var user = _userManager.Users.SingleOrDefault(u => u.UserName == username);
-            var result = await _userManager.AddToRoleAsync(user, roleName);
-
-            return result.Succeeded
-                ? Ok()
-                : Problem(result.Errors.First().Description, null, 500);
+                : NotFound("Username or password incorrect");
         }
 
         [Authorize]
