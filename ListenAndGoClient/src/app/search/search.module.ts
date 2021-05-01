345b7d44b79f888862452e6a0af33c4e95eea99b
Marcus Chok <marcus.chok110@gmail.com>
2021-05-01 16:17:54 -0400

    Update imports and new components

--- ListenAndGoClient/src/app/search/search.module.ts
+++ ListenAndGoClient/src/app/search/search.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { SearchComponent } from './search.component';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
+import { SearchYoutubeComponent } from './search-youtube/search-youtube.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [SearchComponent],
-  imports: [SharedModule, CoreModule],
+  declarations: [SearchComponent, SearchYoutubeComponent],
+  imports: [
+    SharedModule,
+    CoreModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+  ],
 })
 export class SearchModule {}
