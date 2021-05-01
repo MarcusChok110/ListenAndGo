119cf646cb4d845f903fbef28dcc02ad538fecd2
Marcus Chok <marcus.chok110@gmail.com>
2021-05-01 16:16:15 -0400

    Add initial styling to footer component for playing songs

--- ListenAndGoClient/src/app/layout/footer/footer.component.ts
+++ ListenAndGoClient/src/app/layout/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SongService } from '../../core/services/song.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor(public songService: SongService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onTogglePlay(): void {
+    this.songService.togglePlaying();
   }
-
 }
