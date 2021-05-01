f558c0d9a2f06772f68861cec3f2b48ed6bc7b7a
Marcus Chok <marcus.chok110@gmail.com>
2021-05-01 16:14:19 -0400

    Create service for youtube related features and add method to search using the api

---
+++ ListenAndGoClient/src/app/core/services/youtube.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ApiHttpService } from './api-http.service';
+import { YoutubeQueryResponse } from '../models/youtube-query.model';
+import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class YoutubeService {
+  private readonly apiRoute = 'api/Youtube';
+  public searchResults$?: Observable<YoutubeQueryResponse>;
+  constructor(private apiHttpService: ApiHttpService) {}
+
+  search(query: string): void {
+    // params is immutable; either keep it as it is or:
+    //    params = params.set(...)
+    // or
+    //    new HttpParams({ fromString: ...})
+    // but the following does not work:
+    //    params = new ...
+    //    params.set(...) <-- does not change the original params
+    const params = new HttpParams().set('query', query);
+
+    this.searchResults$ = this.apiHttpService.get<YoutubeQueryResponse>(
+      this.apiRoute,
+      params
+    );
+  }
+}
