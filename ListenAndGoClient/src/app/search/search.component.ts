4af7873d84da7bc7338cb6a492cd2d93766bb658
Marcus Chok <marcus.chok110@gmail.com>
2021-05-01 16:18:37 -0400

    Add logic for submitting searches to youtube service

--- ListenAndGoClient/src/app/search/search.component.ts
+++ ListenAndGoClient/src/app/search/search.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../core/services/youtube.service';
+
+const YOUTUBE = 0;
+const SOUNDCLOUD = 0;
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  public query: string;
+  public selected: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public youtubeService: YoutubeService) {
+    this.query = '';
+    this.selected = 0;
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    switch (this.selected) {
+      case YOUTUBE:
+        this.youtubeService.search(this.query);
+        break;
+      case SOUNDCLOUD:
+        console.log(`${this.query} : ${this.selected}`);
+        break;
+    }
+  }
+
+  onReset(): void {
+    this.query = '';
+  }
 }
