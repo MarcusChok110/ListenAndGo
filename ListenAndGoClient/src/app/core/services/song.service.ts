abca3ecd9c656361f99a301e7b305d13451c79ab
Marcus Chok <marcus.chok110@gmail.com>
2021-05-04 23:47:44 -0400

    Create some implementations for song service

--- ListenAndGoClient/src/app/core/services/song.service.ts
+++ ListenAndGoClient/src/app/core/services/song.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Song } from '../models/song.model';
 
 @Injectable()
 export class SongService {
   public isPlaying: boolean;
   public isLooping: boolean;
+  public songQueue: Song[];
+  public currentSong?: Song;
+  public currentSongIndex?: number;
 
   constructor() {
     this.isPlaying = false;
     this.isLooping = false;
+    this.songQueue = [];
+  }
+
+  playSong(song: Song, index: number): void {
+    this.currentSong = song;
+    this.currentSongIndex = index;
   }
 
   togglePlaying(): void {
@@ -16,5 +26,38 @@ export class SongService {
 
   toggleLooping(): void {
     this.isLooping = !this.isLooping;
+  }
+
+  addToQueue(song: Song): void {
+    if (this.queueContains(song)) {
+      return;
+    }
+    this.songQueue.push(song);
+  }
+
+  removeFromQueue(song: Song): void {
+    this.songQueue = this.songQueue.filter((s) => !this.isEqual(s, song));
+  }
+
+  clearQueue(): void {
+    this.songQueue = [];
+  }
+
+  queueContains(song: Song): boolean {
+    for (const s of this.songQueue) {
+      if (this.isEqual(song, s)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isEqual(song1: Song, song2: Song): boolean {
+    return (
+      song1.name === song2.name &&
+      song1.artist === song2.artist &&
+      song1.type === song2.type &&
+      song1.path === song2.path
+    );
   }
 }
