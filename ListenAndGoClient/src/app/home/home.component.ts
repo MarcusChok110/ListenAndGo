39543d7ac30f13d7f2ef16cf1d9c9ac757cfc53a
Marcus Chok <marcus.chok110@gmail.com>
2021-05-04 23:46:10 -0400

    Remove unnecessary code

--- ListenAndGoClient/src/app/home/home.component.ts
+++ ListenAndGoClient/src/app/home/home.component.ts
@@ -1,45 +1,12 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { YouTubePlayer } from '@angular/youtube-player';
-import { Observable, interval, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  public apiLoaded = false;
-  @ViewChild('player', { static: true }) player!: YouTubePlayer;
-  @ViewChild('btn', { static: true }) btn!: HTMLButtonElement;
-  public currentSeconds$?: Observable<number>;
-  public currentSecondsSubscription?: Subscription;
-
+export class HomeComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    if (!this.apiLoaded) {
-      const tag = document.createElement('script');
-      tag.src = 'https://www.youtube.com/iframe_api';
-      document.body.appendChild(tag);
-      this.apiLoaded = true;
-    }
-    this.currentSeconds$ = interval(1000).pipe(
-      map(() => {
-        return this.player.getCurrentTime();
-      })
-    );
-    // this.currentSecondsSubscription = this.currentSeconds$.subscribe(
-    //   console.log
-    // );
-  }
-
-  ngOnDestroy(): void {
-    this.currentSecondsSubscription?.unsubscribe();
-  }
-
-  play(): void {
-    // this.player.nativeElement.playVideo();
-    console.log(this.player.playVideo());
-  }
+  ngOnInit(): void {}
 }
