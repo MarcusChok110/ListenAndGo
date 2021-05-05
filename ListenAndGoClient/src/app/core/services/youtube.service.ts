3c57cd064d4825bd4cb0ef81d1d09f00585e5f75
Marcus Chok <marcus.chok110@gmail.com>
2021-05-04 23:47:16 -0400

    Extract conversion from youtube item to song logic to service

--- ListenAndGoClient/src/app/core/services/youtube.service.ts
+++ ListenAndGoClient/src/app/core/services/youtube.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ApiHttpService } from './api-http.service';
-import { YoutubeQueryResponse } from '../models/youtube-query.model';
+import {
+  YoutubeItem,
+  YoutubeQueryResponse,
+} from '../models/youtube-query.model';
 import { Observable } from 'rxjs';
 import { HttpParams } from '@angular/common/http';
+import { Song } from '../models/song.model';
+import { DecodeHTMLPipe } from '../../shared/decode-html.pipe';
 
 @Injectable()
 export class YoutubeService {
   private readonly apiRoute = 'api/Youtube';
   public searchResults$?: Observable<YoutubeQueryResponse>;
-  constructor(private apiHttpService: ApiHttpService) {}
+
+  constructor(
+    private apiHttpService: ApiHttpService,
+    private decodeHTMLPipe: DecodeHTMLPipe
+  ) {}
 
   search(query: string): void {
     // params is immutable; either keep it as it is or:
@@ -24,5 +33,16 @@ export class YoutubeService {
       this.apiRoute,
       params
     );
+  }
+
+  itemToSong(item: YoutubeItem): Song {
+    return {
+      name: this.decodeHTMLPipe.transform(item.snippet.title),
+      artist: item.snippet.channelTitle,
+      path: item.id.videoId,
+      type: 'YOUTUBE',
+      releaseDate: item.snippet.publishedAt,
+      imgUrl: item.snippet.thumbnails.default.url,
+    } as Song;
   }
 }
